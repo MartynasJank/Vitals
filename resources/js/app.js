d8b7b8e7bcbea58277904e260e310fd33b996ad5
Martynas Jankauskas <martynasjank@gmail.com>
2026-05-11 12:40:47 +0300

    chore: bundle highlight.js via npm instead of CDN

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-//
+import hljs from 'highlight.js/lib/core';
+import nginx from 'highlight.js/lib/languages/nginx';
+import bash from 'highlight.js/lib/languages/bash';
+
+hljs.registerLanguage('nginx', nginx);
+hljs.registerLanguage('bash', bash);
+
+window.hljs = hljs;
